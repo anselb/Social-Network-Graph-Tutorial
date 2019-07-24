5eebe606b5b71a8d487ce39164bd8e4b31b89dd9
Ansel Bridgewater <iamansel@gmail.com>
2019-07-23 23:46:37 -0700

    Added test for shortest_path

--- Graph-Tutorial/graph_test.py
+++ Graph-Tutorial/graph_test.py
@@ -383,6 +383,14 @@ class GraphTest(unittest.TestCase):
         no_path = g.find_shortest_path("A", "A")
         self.assertEqual(no_path, None)
 
+        # No path to vertex that does not have an edge directed into it
+        g.directed = True
+        # Added directed edge from 0 to A
+        g.add_edge(0, "A")
+        # Try to get 0 from A
+        no_directed_path = g.find_shortest_path("A", 0)
+        self.assertEqual(no_directed_path, None)
+
         # Error should be raised when vertex not in graph
         with self.assertRaises(KeyError):
             g.find_shortest_path("A", 1)
